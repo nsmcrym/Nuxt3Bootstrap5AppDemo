577e6d73a7c9c10f63ca93a2a45179a60cbec013
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-10-31 04:52:19 +0000

    2023/10/31

--- pages/kari.vue
+++ pages/kari.vue
@@ -2,7 +2,20 @@
   <div class="container">
     <h1>タイトル</h1>
   </div>
-<h2>テーマ（思いつかない）</h2>
- <p>〇〇についての説明</p>
- <p>仮表示</p>
-</template>
+  <div class="container">
+    <h2>テーマ（思いつかない）</h2>
+    <p>〇〇についての説明</p>
+    <p>仮表示</p>
+  </div>
+  <div class="container">
+    <h2>テーマについての深堀</h2>
+    <p>仮表示</p>
+  </div>
+  <div class="container">
+    <h2>深堀２</h2>
+    <p>仮表示</p>
+  </div>
+  <div class="container">
+    <h2>まとめ</h2>
+    <p>仮表示</p>
+  </div></template>
