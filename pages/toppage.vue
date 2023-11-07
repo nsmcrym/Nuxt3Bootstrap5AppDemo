5b930596ac07c09841d0483c4f226e94ffd2716a
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-11-07 05:59:15 +0000

    tp

---
+++ pages/toppage.vue
@@ -0,0 +1,18 @@
+<templete>
+<section class = "wrapper">
+<div class = "container">
+<h1>"Webフロントエンド開発実習"</h1>
+</section>
+<p>Webフロントエンド開発の授業で作ったサイトです</p>
+</div>
+</templete>
+<script>
+
+</script>
+<style>
+.wrapper .container{
+    max-width:1000px;
+    margin:0px auto;
+    padding:80px 0px;
+}
+</style>
