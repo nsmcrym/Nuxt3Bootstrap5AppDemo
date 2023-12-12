b57c4dffddd3e45094d7fa26afad01ce02e57ab2
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-12-12 05:54:19 +0000

    内容を決めた

--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container">
-    <h1>こんにちは</h1>
-  </div>
-</template>
--- pages/timer.vue
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<body>
-<div class="timer-container">
-<h2>頑張りましょう</h2>
-<div class="time-container">
-</div>
-</body>
