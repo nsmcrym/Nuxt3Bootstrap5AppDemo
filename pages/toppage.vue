7f5aabd1ea7747a853dfcd113813536264d0d219
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-12-12 06:02:02 +0000

    あ

--- pages/toppage.vue
+++ pages/toppage.vue
@@ -1,27 +1,29 @@
 <template>
-<h1>サーモンランまとめ</h1>
-<dev class = "container text-center">
-<h2>この記事の目的</h2>
-<p>サイトを作る練習。ついでに2023年冬時点でのサモランの自分の考えをまとめてみる</p>
-</dev>
-<dev class = "container text-center">
-<h2>自分の情報</h2>
-<ul>
-<li>完全野良バイター（以下の内容はすべて野良）</li>
-<li>全ステージカンスト（執筆時点）</li>
-<li>最高納品数211(タラポ)（昼のみ188）</li>
-<li>ビッグランall金、バチコン三回目以外金（三回目は不参加）</li>
-</ul>
-</dev>
-<dev class = "container text-center">
-<h2>目次</h2>
-<dev class = "row">
-<dev class = "col">ステージ</dev>
-<dev class = "col">シャケ</dev>
-</dev>
-<dev class = "row">
-<dev class = "col">スペシャル</dev>
-<dev class = "col">立ち回り</dev>
-</dev>
-</dev>
+    <h1>サーモンランまとめ</h1>
+    <dev class="container text-center">
+        <h2>この記事の目的</h2>
+        <p>サイトを作る練習。ついでに2023年冬時点でのサモランの自分の考えをまとめてみる</p>
+    </dev>
+    <dev class="container text-center">
+        <h2>自分の情報</h2>
+        <ul>
+            <li>完全野良バイター（以下の内容はすべて野良）</li>
+            <li>全ステージカンスト（執筆時点）</li>
+            <li>最高納品数211(タラポ)（昼のみ188）</li>
+            <li>ビッグランall金、バチコン三回目以外金（三回目は不参加）</li>
+        </ul>
+    </dev>
+    <dev class="container text-center">
+        <h2>目次</h2>
+        <dev class="row">
+            <dev class="col">ステージ</dev>
+            <dev class="col">シャケ</dev>
+            <dev class="col">スペシャル</dev>
+        </dev>
+        <dev class="row">
+            <dev class="col">立ち回り</dev>
+            <dev class="col">備考</dev>
+        </dev>
+
+    </dev>
 </template>
