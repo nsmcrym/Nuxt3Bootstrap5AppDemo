342e9a61e7e9af86b020ec01ca8c56cc88b8dae4
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-12-13 07:22:40 +0000

    a

--- pages/salmon.vue
+++ pages/salmon.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div class = "container">
+    <div class = "row">
+    <div class = "col-4">
+    a
+    </div>
+    <div class = "col-4">
+    a
+    </div>
+    <div class = "col-4">
+    a
+    </div>
+    </div>
+    </div>
 </template>
--- pages/toppage.vue
+++ pages/toppage.vue
@@ -1,10 +1,10 @@
 <template>
-    <h1>サーモンランまとめ</h1>
-    <dev class="container text-center">
+    <h1 class = "text-center">サーモンランまとめ</h1>
+    <div class="container text-center">
         <h2>この記事の目的</h2>
         <p>サイトを作る練習。ついでに2023年冬時点でのサモランの自分の考えをまとめてみる</p>
-    </dev>
-    <dev class="container text-center">
+    </div>
+    <div class="container text-center">
         <h2>自分の情報</h2>
         <ul>
             <li>完全野良バイター（以下の内容はすべて野良）</li>
@@ -12,18 +12,28 @@
             <li>最高納品数211(タラポ)（昼のみ188）</li>
             <li>ビッグランall金、バチコン三回目以外金（三回目は不参加）</li>
         </ul>
-    </dev>
-    <dev class="container text-center">
+    </div>
+    <div class="container text-center">
         <h2>目次</h2>
-        <dev class="row">
-            <dev class="col">ステージ</dev>
-            <dev class="col">シャケ</dev>
-            <dev class="col">スペシャル</dev>
-        </dev>
-        <dev class="row">
-            <dev class="col">立ち回り</dev>
-            <dev class="col">備考</dev>
-        </dev>
-
-    </dev>
+        <a href="#" class="">
+        <img src="">
+        <div>ステージ</div>
+        </a>
+        <a href="#" class="">
+        <img src="">
+        <div>シャケ</div>
+        </a>
+        <a href="#" class="">
+        <img src="">
+        <div>スペシャル</div>
+        </a>
+        <a href="#" class="">
+        <img src="">
+        <div>特殊WAVE</div>
+        </a>
+        <a href="#" class="">
+        <img src="">
+        <div>おまけ</div>
+        </a>
+    </div>
 </template>
