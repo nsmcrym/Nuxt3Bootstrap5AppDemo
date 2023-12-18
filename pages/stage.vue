2db62049acdb35d9902c0c7c10df4d24f7ce9583
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-12-18 05:27:07 +0000

    a

--- pages/stage.vue
+++ pages/stage.vue
@@ -2,39 +2,39 @@
     <h2 class="text-center">ステージ解説</h2>
     <h3 class=" text-center">ここでは自分が大切だと思っていることや難易度をステージごとに主観で書いていきます</h3>
 
-<div class = "container text-center">
-<h3>シェケナダム（私的難易度 4.5/5.0）</h3>
-<img scr = "">
-<p>シェケナダム（以下ダム）は</p>
-</div>
+    <div class="container text-center">
+        <h3>シェケナダム（私的難易度 4.5/5.0）</h3>
+        <img scr="">
+        <p>シェケナダム（以下ダム）は</p>
+    </div>
 
-<div class = "container text-center">
-<h3>アラマキ砦（私的難易度 3.75/5.0）</h3>
-<img scr = "">
-<p>アラマキ砦（以下アラマキ）は</p>
-</div>
+    <div class="container text-center">
+        <h3>アラマキ砦（私的難易度 3.75/5.0）</h3>
+        <img scr="">
+        <p>アラマキ砦（以下アラマキ）は</p>
+    </div>
 
-<div class = "container text-center">
-<h3>ムニエール海洋発電所（私的難易度 3.5/5.0）</h3>
-<img scr = "">
-<p>ムニエール海洋発電所（以下ムニエル）は</p>
-</div>
+    <div class="container text-center">
+        <h3>ムニエール海洋発電所（私的難易度 3.5/5.0）</h3>
+        <img scr="">
+        <p>ムニエール海洋発電所（以下ムニエル）は</p>
+    </div>
 
-<div class = "container text-center">
-<h3>難破船ドンブラコ（私的難易度 5.0/5.0）</h3>
-<img scr = "">
-<p>難破船ドンブラコ（以下ドンブラコ）は</p>
-</div>
+    <div class="container text-center">
+        <h3>難破船ドンブラコ（私的難易度 5.0/5.0）</h3>
+        <img scr="">
+        <p>難破船ドンブラコ（以下ドンブラコ）は</p>
+    </div>
 
- <div class = "container text-center">
-<h3>すじこジャンクション跡（私的難易度 3.0/5.0）</h3>
-<img scr = "">
-<p>すじこジャンクション跡（以下すじこ）は</p>
-</div>
+    <div class="container text-center">
+        <h3>すじこジャンクション跡（私的難易度 3.0/5.0）</h3>
+        <img scr="">
+        <p>すじこジャンクション跡（以下すじこ）は</p>
+    </div>
 
-<div class = "container text-center">
-<h3>トキシラズいぶし工房（私的難易度 4.0/5.0）</h3>
-<img scr = "">
-<p>トキシラズいぶし工房（以下トキシラズ）は</p>
-</div>
+    <div class="container text-center">
+        <h3>トキシラズいぶし工房（私的難易度 4.0/5.0）</h3>
+        <img scr="">
+        <p>トキシラズいぶし工房（以下トキシラズ）は</p>
+    </div>
 </template>
