d4ecd6a22e4b10f7ee6dcd2f6c25d3b892dfcd65
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2023-12-28 17:00:01 +0000

    あ

--- pages/toppage.vue
+++ pages/toppage.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="container text-center">
         <h2>前提</h2>
-        <p>基本的な仕様や用語はわかるものとする。<br>オカシラ戦については記述しない。
+        <p>基本的な仕様や用語はわかるものとする。<br>オカシラ戦については記述しない。<br>特殊WAVEについては記述しない
         </p>
     </div>
     <div class="container text-center">
