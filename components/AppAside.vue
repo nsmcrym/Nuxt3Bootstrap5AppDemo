6ca4a709a70e27164f8fc0ca05fba023e0362869
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2024-01-23 05:55:29 +0000

    a

--- components/AppAside.vue
+++ components/AppAside.vue
@@ -1,4 +1,25 @@
 <template>
-<a href="/toppage" class="">
-<p>top</p></a>
-</template>
+    <div class="container">
+        <div class="bg-success p-2" style="--bs-bg-opacity: .2;">
+            <a href="/toppage" class="">
+                <div style="padding-left: 30px;">
+                    <h3>top</h3>
+                </div>
+            </a>
+            <a href="/salmon" class="">
+                <div style="padding-left: 30px;">
+                    <h3>シャケ</h3>
+                </div>
+            </a>
+            <a href="/sp" class="">
+                <div style="padding-left: 30px;">
+                    <h3>sp</h3>
+                </div>
+        </a>
+        <a href="/toppage" class="">
+            <div style="padding-left: 30px;">
+                <h3>top</h3>
+            </div>
+        </a>
+    </div>
+</div></template>
