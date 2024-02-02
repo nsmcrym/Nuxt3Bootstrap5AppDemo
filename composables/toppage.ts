db45c17963862a81246a1674c66b55bcc99e1484
nsmcrym <144088307+nsmcrym@users.noreply.github.com>
2024-02-02 20:56:47 +0000

    a

--- composables/toppage.ts
+++
@@ -1,5 +0,0 @@
-export const usetoppage = () => useState("toppage", () => {
-    return{
-        title: "目次",
-    }
-})
--- pages/toppage.vue
+++ pages/toppage.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="container text-center">
-        <h2>{{ data.title }}</h2>
-        <a href="/salmon" class="">
-            <img src="">
+        <h2>目次</h2>
+        <a href="/salmon" >
+  
             <h2>シャケ</h2>
         </a>
-        <a href="/stage" class="">
-            <img src="">
+        <a href="/stage">
+
             <h2>ステージ</h2>
         </a>
-        <a href="/sp" class="">
-            <img src="">
+        <a href="/sp" >
+     
             <h2>スペシャル</h2>
         </a>
 
